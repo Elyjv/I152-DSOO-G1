43766067e74ca24529c65c90e8ac6b2470c61f6d
Alejandro <ferreyra.alejan@gmail.com>
2022-11-07 19:12:15 -0300

    Proceso Registro y Login

---
+++ Commerces/app/src/main/java/com/af/commerces/loginActivity.java
@@ -0,0 +1,14 @@
+package com.af.commerces;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class loginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
